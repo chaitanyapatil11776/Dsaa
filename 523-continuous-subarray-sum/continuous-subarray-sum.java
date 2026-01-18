3d0e9073ddeb1669b8ce36e628cc599dfa7eac12
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-18 13:36:59 +0400

    Time: 23 ms (93.68%) | Memory: 140 MB (23.34%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,27 +1,20 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-
-        HashMap<Integer, Integer> ss = new HashMap<>();
-        ss.put(0, -1);   // remainder 0 at index -1
-
-        int sum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-
-            int rem = (k == 0) ? sum : sum % k;
-
-            if (ss.containsKey(rem)) {
-                int index = ss.get(rem);
-                if (i - index >= 2) {
-                    return true;
-                }
-            } else {
-                ss.put(rem, i);   // store FIRST occurrence only
-            }
+    int sum=0;
+    HashMap<Integer,Integer>ss=new HashMap<>()    ;
+    ss.put(0,-1);
+    for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+        int rem=(k==0)?sum:sum % k;
+        if(ss.containsKey(rem)){
+            int index=ss.get(rem);
+            if(i-index>=2)return true;
+          
         }
-        return false;
+          else{
+                ss.put(rem,i);
+            }
+    }
+    return false;
     }
 }
