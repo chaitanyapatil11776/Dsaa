67090d67959c958ed74d8cbec2ae9c7d21506d88
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-28 20:52:32 +0400

    Time: 0 ms (100.00%) | Memory: 43.2 MB (17.12%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())return false;
+        String cc=s+s;
+        return cc.contains(goal);
+    }
+}
