55cbab73ab00f21e538b03cbcee4001fbe693f1f
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-01 10:18:01 +0530

    Time: 22 ms (95.90%) | Memory: 49.7 MB (5.41%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,25 +1,22 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n=nums.length;
-                int p[]=new int[n];
-                int count=0;
-                p[0]=nums[0];
-                HashMap<Integer,Integer>ss=new HashMap<>();
-                ss.put(0,1);
-                for(int i=1;i<n;i++){
-                    p[i]=p[i-1]+nums[i];
-                }
-                for(int i=0;i<n;i++){
-                   
-                    int val=p[i]-k;
-                    if(ss.containsKey(val)){
-                        count+=ss.get(val);
-                
-                    }
-                    ss.put(p[i],ss.getOrDefault(p[i],0)+1);
-                }
-                return count;
+     int n   =nums.length;
+     int p[]=new int[n];
+     int count=0;
+     p[0]=nums[0];
+     for(int i=1;i<n;i++){
+        p[i]=p[i-1]+nums[i];
+     }
 
-
+     HashMap<Integer,Integer>ss=new HashMap<>();
+     ss.put(0,1);
+     for(int i=0;i<n;i++){
+        int val=p[i]-k;
+        if(ss.containsKey(val)){
+            count+=ss.get(val);
+        }
+        ss.put(p[i],ss.getOrDefault(p[i],0)+1);
+     }
+     return count;
     }
 }
