e2234c33934baac366cac5d6900cab5a33e591fa
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-20 08:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (60.64%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,15 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
-     int c0=0,c1=0,c2=0;
-     for(int x:nums){
-        if(x==0)c0++;
-        else if(x==1)c1++;
-        else c2++;
-
-     }  
-     int index=0;
-     while(c0-->0)nums[index++]=0;
-     while(c1-->0)nums[index++]=1;
-     while(c2-->0)nums[index++]=2; 
+        int c0=0,c1=0,c2=0;
+        for(int n:nums){
+            if(n==0)c0++;
+            if(n==1)c1++;
+            if(n==2)c2++;
+        }
+        int index=0;
+        while(c0-->0)nums[index++]=0;
+        while(c1-->0)nums[index++]=1;
+        while(c2-->0)nums[index++]=2;
     }
 }
