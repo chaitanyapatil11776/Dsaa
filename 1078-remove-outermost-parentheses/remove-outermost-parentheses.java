c93807352d75724d60a5be27ac71fdd93b93f16a
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-14 00:41:52 +0530

    Time: 8 ms (20.17%) | Memory: 43.1 MB (98.01%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character>ss=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='('){
+                if(ss.size()>0){
+                    sb.append(c);
+                }ss.push(c);
+            }else{
+                ss.pop();
+                if(ss.size()>0){
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
