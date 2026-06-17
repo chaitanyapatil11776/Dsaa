ac4989e100fec18ac6e22c3b447af86b7178f228
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-18 00:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (15.70%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -14,10 +14,12 @@ class Solution {
         while(curr!=null && curr.next!=null){
             if(curr.val==curr.next.val){
                 curr.next=curr.next.next;
+
             }else{
                 curr=curr.next;
             }
         }
         return head;
+
     }
 }
