be05844fa0960e7ca342cef9355be605a412c8a7
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-20 22:50:43 +0530

    Time: 1 ms (99.94%) | Memory: 94.5 MB (50.19%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minp=prices[0];
+        int maxp=0;
+        for(int i=0;i<prices.length;i++){
+            minp=Math.min(minp,prices[i]);
+            maxp=Math.max(maxp,prices[i]-minp);
+        }
+        return maxp;
+    }
+}
