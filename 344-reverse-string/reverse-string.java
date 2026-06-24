8327d6ba08c50d3d1f4fac4b7d951681c5987560
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-06-24 11:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (54.48%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,12 +3,14 @@ class Solution {
         int left=0;
         int right=s.length-1;
         while(left<right){
-        char temp=s[left];
+            char temp=s[left];
             s[left]=s[right];
             s[right]=temp;
             left++;
             right--;
         }
-       
+
+
+      
     }
 }
