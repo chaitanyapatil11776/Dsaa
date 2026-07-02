432ef8d018156f831c97ce9db7fcc47cdac8ba86
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-07-02 10:24:00 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (95.84%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -15,20 +15,12 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-
-        if (root == null) return 0;
-
-        // Ignore left subtree
-        if (root.val < low)
-            return rangeSumBST(root.right, low, high);
-
-        // Ignore right subtree
-        if (root.val > high)
-            return rangeSumBST(root.left, low, high);
-
-        // Value inside range
-        return root.val
-                + rangeSumBST(root.left, low, high)
-                + rangeSumBST(root.right, low, high);
+        if(root==null)return 0;
+        if(root.val<low){
+            return rangeSumBST(root.right,low,high);
+        }if(root.val>high){
+            return rangeSumBST(root.left,low,high);
+        }
+        return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
     }
 }
