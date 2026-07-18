75a33543d88d7a2a6652063388881ec822a64953
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-07-18 16:27:29 +0530

    Time: 3 ms (99.85%) | Memory: 94.2 MB (79.97%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,8 +10,10 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+
         ListNode s=head;
         ListNode f=head;
+
         while(f!=null && f.next!=null){
             s=s.next;
             f=f.next.next;
@@ -19,19 +21,24 @@ class Solution {
         }
         ListNode prv=null;
         ListNode next=null;
+      
         while(s!=null){
             next=s.next;
             s.next=prv;
             prv=s;
             s=next;
-     
+
         }
+
+
         while(prv!=null){
             if(prv.val!=head.val){
                 return false;
-            }head=head.next;
+            }
             prv=prv.next;
+            head=head.next;
         }
         return true;
+
     }
 }
