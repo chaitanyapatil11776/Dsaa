1784700d5605a4b798fdea2938aa4d251b0f53d2
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-21 16:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (50.44%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -12,18 +12,19 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode s=head;
+        
         ListNode f=head;
         while(f!=null && f.next!=null){
             s=s.next;
             f=f.next.next;
             if(s==f)return ss(head,f);
-
-        }return null;
+        }
+        return null;
 
     }
 
 
-    ListNode ss(ListNode head,ListNode f){
+    ListNode ss(ListNode  head,ListNode f ){
         while(head!=f){
             head=head.next;
             f=f.next;
