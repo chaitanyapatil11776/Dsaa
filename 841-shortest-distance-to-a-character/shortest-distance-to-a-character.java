4fe778b2fcb107996b6194b11cb8eaed419901b6
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-22 13:32:44 +0530

    Time: 24 ms (8.46%) | Memory: 44.1 MB (33.04%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -4,13 +4,15 @@ class Solution {
         int p[]=new int[n];
         for(int i=0;i<n;i++){
             int min=Integer.MAX_VALUE;
+
+
+
             for(int j=0;j<n;j++){
                 if(s.charAt(j)==c){
                     min=Math.min(min,Math.abs(i-j));
                 }
-            }
-            p[i]=min;
-           
+
+            }p[i]=min;
         }
         return p;
     }
