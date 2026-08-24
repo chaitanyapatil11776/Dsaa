51e36259160c58740d9c57bdf0ce2b9e10eb6d0c
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-08-24 12:30:47 +0530

    Time: 5 ms (98.30%) | Memory: 43.1 MB (32.72%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,20 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
         int count=0;
-        for(int i=0;i<s.length();i++){
-            count+=expand(s,i,i);
-            count+=expand(s,i,i+1);
-        }
-        return count;
+       for(int i=0;i<s.length();i++){
+        count+=count(s,i,i);
+        count+=count(s,i,i+1);
+       }
+       return count;
     }
 
 
-    int expand(String s,int l,int r){
+    int count(String s,int l,int r){
         int count=0;
         while(l>=0 && r<s.length()  && s.charAt(l)==s.charAt(r)){
             count++;
             l--;
-            r++;
+           r++;
+            
+            
         }
         return count;
     }
